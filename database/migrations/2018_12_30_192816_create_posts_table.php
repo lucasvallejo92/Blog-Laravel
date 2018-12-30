e52b3bec35a9e170dc43c8dde31fda56a30a077e
Lucas Vallejo <lucasvallejo92@gmail.com>
2018-12-30 17:12:26 -0300

    Is generated and configured all the resources for the model
    - Is maked the relations on the database. - Is created the resources controllers and factories but they aren't configured

---
+++ database/migrations/2018_12_30_192816_create_posts_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+
+            $table->string('name', 128); // Post name. "Post de Interes"
+            $table->string('slug', 128)->unique(); // Friendly URL. "pagina.com/post-de-interes"
+
+            $table->mediumText('excerpt')->nulleable();
+            $table->text('body');
+            $table->enum('status', ['PUBLISHED', 'DRAFT'])->default('DRAFT');
+            $table->string('file', 128)->nulleable();
+
+            $table->timestamps();
+
+            //Relation
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade'); // onDelete and onUpdate: modify on cascade all relations
+
+            $table->foreign('category_id')->references('id')->on('categories')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
